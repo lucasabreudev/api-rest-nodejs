8988e1e6b68076f967ce4290769790fe42d464bb
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-03-10 16:35:47 -0300

    feat: testando criação de transação

--- src/test/example.spec.ts
+++ src/test/example.spec.ts
@@ -1,7 +1,22 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
 
-test('', () => {
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toBe(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 100,
+      type: 'credit',
+    })
+    .expect(201)
 })
