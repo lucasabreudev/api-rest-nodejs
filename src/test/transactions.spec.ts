fc3d98be35d7ac381a9dce52f959be5571b5a773
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-03-10 21:04:35 -0300

    feat: categorizando os testes

---
+++ src/test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { afterAll, beforeAll, it, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
+
+describe('Transaction routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 100,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
